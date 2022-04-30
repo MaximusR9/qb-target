f876be231d0c59591bec96dd42ea1d8b98797fed
MaximusR9 <49029210+MaximusR9@users.noreply.github.com>
2022-04-30 18:18:59 +0500

    Updated to NP style

--- html/js/app.js
+++ html/js/app.js
@@ -3,9 +3,10 @@ const Targeting = Vue.createApp({
         return {
             Show: false, // leave this
             ChangeTextIconColor: false, // This is if you want to change the color of the icon next to the option text with the text color
-            StandardEyeIcon: "far fa-eye", // This is the default eye icon
+            StandardEyeIcon: "https://cdn.discordapp.com/attachments/903021216464531507/903024370665000981/normaleye.png", // This is the default eye icon
             CurrentIcon: this.StandardEyeIcon, // leave this
-            SuccessColor: "rgb(30, 144, 255)", // This is the color when the target has found the option
+            SuccessIcon: 'https://cdn.discordapp.com/attachments/903021216464531507/903024373626208336/activeeye.png', // Instead of icon it's using a image source found in HTML
+            SuccessColor: "rgb(5, 241, 178)", // This is the color when the target has found the option
             StandardColor: "white", // This is the standard color, change this to the same as the StandardColor if you have changed it
             TargetEyeStyleObject: {
                 color: this.StandardColor, // leave this
@@ -116,7 +117,7 @@ const Targeting = Vue.createApp({
 
         FoundTarget(item) {
             if (item.data) this.CurrentIcon = item.data;
-            else this.CurrentIcon = this.StandardEyeIcon;
+            else this.CurrentIcon = this.SuccessIcon;
             this.TargetEyeStyleObject.color = this.SuccessColor;
         },
 
